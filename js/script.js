b380e04b4936915e79f2461342a0c2480ba0ecf0
KopibatteSalutabang <134840536+KopibatteSalutabang@users.noreply.github.com>
2023-05-28 18:32:39 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Toggle Class Active
+const navbarNav = document.querySelector(".navbar-nav");
+// ketika hamburger menu diklik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// klik diluar side bar untuk menghilangkan sidebar
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
